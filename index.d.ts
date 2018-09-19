40e45782f87c4ca4a2a4be6c4f99296085839889
Alex Natashkin <natashkinsasha@gmail.com>
2018-09-19 23:44:23 +0300

    v 1.0.6

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+import * as express from "express";
+
+
+declare namespace queryParser {
+    interface Options {
+        parseBoolean?: boolean,
+        parseNumber?: boolean,
+    }
+}
+
+declare function queryParser(options: queryParser.Options): (req: express.Request,
+                                                        res: express.Response,
+                                                        next: express.NextFunction,) => void;
+
+
+export = queryParser
+
+
+
+
+
